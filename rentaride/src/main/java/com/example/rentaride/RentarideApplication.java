228fe997443119bd04a11714e6fb7f76921336dd
Thanush <asthanush10@gmail.com>
2025-10-06 22:45:20 +0530

    Describe your update here

---
+++ rentaride/src/main/java/com/example/rentaride/RentarideApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentaride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentarideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentarideApplication.class, args);
+	}
+
+}
---
+++ rentaride/src/test/java/com/example/rentaride/RentarideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentaride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentarideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
